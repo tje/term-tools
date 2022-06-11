0503ecdb1a9b7524cac5c8b04cbf0e7ea7abffa0
modem <tyler@modem.io>
2022-06-11 14:35:11 -0700

    add speed gate to spinner generator

--- src/progress_bar.ts
+++ src/progress_bar.ts
@@ -12,20 +12,37 @@ export function progressBar (width: number, progress: number, characters: string
   return barFill.padEnd(width, characters[characters.length - 1])
 }
 
+type PrimitiveHint = 'number' | 'string' | 'default'
 
-function * createSpinner (characters: string) {
+function * createSpinGenerator (characters: string, speed = 0) {
   const chars = characters.split('')
+  let ts = Date.now()
   while (true) {
     const char = chars.shift()!
     chars.push(char)
-    yield char
+    while (true) {
+      yield char
+      if (Date.now() > ts + speed) {
+        ts = Date.now()
+        break
+      }
+    }
   }
 }
-export function spinner (characters: string = PROGRESS_SPIN.BASIC) {
-  const gen = createSpinner(characters)
+export function spinner (characters: string = PROGRESS_SPIN.BRAILLE_LINEAR, speed = 50) {
+  const gen = createSpinGenerator(characters, speed)
   return {
-    [Symbol.toPrimitive] () {
+    [Symbol.toPrimitive] (hint: PrimitiveHint) {
+      switch (hint) {
+        case 'default':
+        case 'string':
+          return this.value
+        case 'number':
+          return NaN
+      }
+    },
+    get value () {
       return gen.next().value
-    }
+    },
   }
 }
